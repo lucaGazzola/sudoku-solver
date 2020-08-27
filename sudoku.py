98ed9fd2f50b3a4c287ae9812f8404206384c048
Luca Gazzola <luca.gazzola@lastminute.com>
2020-08-27 20:28:12 +0200

    refactored with backtracking algorithm, simplified input parsing

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import genfromtxt
 
 def square(sudoku,k,j):
     if k < 3 and j < 3:
@@ -20,53 +21,22 @@ def square(sudoku,k,j):
     if k >= 6 and k < 9 and j >= 6 and j < 9:
         return sudoku[6:9, 6:9]
 
+def is_possible(sudoku,i,j,n):
+    return n not in sudoku[i] and n not in sudoku[:,j] and n not in square(sudoku,i,j)
 
 def fill(sudoku):
-    tmp = np.array(sudoku)
-    possibilities = {}
-    flag = False
     for k in range(0,len(sudoku)):
         for j in range (0,len(sudoku[k])):
-            if sudoku[k][j] == 'e':
+            if sudoku[k][j] == 0:
                 for i in range(1,10):
-                    if str(i) not in sudoku[k] and str(i) not in sudoku[:,j] and str(i) not in square(sudoku,k,j):
-                        flag = True
-                        if (k,j) not in possibilities:
-                            possibilities[(k,j)] = [str(i)]
-                        else:
-                            possibilities[(k,j)].append(str(i))
-    
-    for k in range(0,len(sudoku)):
-        for j in range (0,len(sudoku[k])):
-            if (k,j) in possibilities:
-                if len(possibilities[(k,j)]) == 1:
-                    sudoku[k][j] = possibilities[(k,j)][0]
-    
-    if flag:
-        return fill(sudoku)
-    else:
-        print('sudoku filled.')
-        return sudoku
-
-def load(filename):
-    print('loading sudoku...')
-    sudoku = []
-    sudokuFile = open(filename, 'r')
-    lines = sudokuFile.readlines() 
-    for line in lines:
-        line = line.strip("\n") 
-        sudoku.append(line.split(','))
-    print('sudoku loaded.')
-    return np.array(sudoku)
-
-def print_formatted(sudoku):
-    for row in sudoku:
-        for cell in row:
-            print("{}|".format(cell if cell != 'e' else ' '),end='')
-        print('')
+                    if is_possible(sudoku,k,j,i):
+                        sudoku[k][j] = i
+                        fill(sudoku)
+                        sudoku[k][j] = 0
+                return
+    print(sudoku)
 
 if __name__ == '__main__':
-    sudoku = load('sudoku_easy2.txt')
-    print_formatted(sudoku)
-    solved_sudoku = fill(sudoku)
-    print_formatted(solved_sudoku)
+    sudoku = genfromtxt('sudoku_hard.csv', delimiter=',')
+    print(sudoku)
+    fill(sudoku)
